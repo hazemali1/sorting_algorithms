d8535fdac87e97e31940ec628139d5956ed737ea
hazemali1 <hazemali1343@gmail.com>
2023-07-22 05:54:20 -0700

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -6,8 +6,11 @@
  * @array: Array should be sorted
  * @s: first
  * @d: last
+ * @size: Size of array
+ *
+ * Return: Int
 */
-void help(int *array, int s, int d)
+int help(int *array, int s, int d, size_t size)
 {
 	int q, w, f, e;
 
@@ -21,11 +24,13 @@ void help(int *array, int s, int d)
 			e = array[w];
 			array[w] = array[f];
 			array[f] = e;
+			print_array(array, size);
 		}
 	}
 	e = array[w + 1];
 	array[w + 1] = array[d];
 	array[d] = e;
+	print_array(array, size);
 	return (w + 1);
 }
 
@@ -35,16 +40,17 @@ void help(int *array, int s, int d)
  * @array: Array should be sorted
  * @s: first
  * @d: last
+ * @size: Size of array
 */
-void quick(int *array, int s, int d)
+void quick(int *array, int s, int d, size_t size)
 {
 	int q;
 
 	if (s < d)
 	{
-		q = help(array, s, d);
-		quick(array, s, q - 1);
-		quick(array, q + 1, d)
+		q = help(array, s, d, size);
+		quick(array, s, q - 1, size);
+		quick(array, q + 1, d, size);
 	}
 }
 
@@ -58,5 +64,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (size < 2)
 		return;
-	quick(array, 0, size - 1);
+	quick(array, 0, size - 1, size);
 }
