d92b94bd39fbeac04e078212e16535e0dfa84cdd
hazemali1 <hazemali1343@gmail.com>
2023-07-22 05:09:56 -0700

    selction sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Selction sort
+ *
+ * @array: Array sould be sorted
+ * @size: Size of array
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t s, d, q;
+	int w;
+
+	if (size < 2)
+		return;
+	for (s = 0; s < n - 1; s++)
+	{
+		q = i;
+		for (d = s + 1; d < n; d++)
+		{
+			if (array[d] < array[q])
+				q = d;
+		}
+		if (q != i)
+		{
+			w = array[q];
+			array[q] = array[i];
+			array[i] = w;
+		}
+	}
+}
