78f299846c0661fb872cdbd9ef1da8242d525bbc
hazemali1 <hazemali1343@gmail.com>
2023-07-22 03:54:25 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sort
+ *
+ * @array: Array sould sort
+ * @size: Size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t s, d;
+	int q;
+
+	if (size < 2)
+		return;
+	for (s = 0; s < size; s++)
+	{
+		for (d = 0; d < size; d++)
+		{
+			if (array[d] > array[d + 1] && array[d + 1])
+			{
+				q = array[d];
+				array[d] = array[d + 1];
+				array[d + 1] = q;
+				print_array(array, size);
+			}
+		}
+	}
+}
